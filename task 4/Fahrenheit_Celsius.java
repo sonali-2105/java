67043aad46bd2c6237f05e7489bf28c3e0e29fd1
sonali <sonali_chichghare@vistasoftwares.com>
2020-02-15 13:21:27 +0530

    add new files

---
+++ task 4/Fahrenheit_Celsius.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Fahrenheit_Celsius{
+    public static void main(String[] args){
+        Double Fahrenheit,Celsius;
+        Scanner scan=new Scanner(System.in);
+        Fahrenheit=i.nextDouble();
+             for(int i=1; i<=20; i++){
+                 for(int j=1; j<=i; j=i+20){
+                     System.out.print(((Fahrenheit-32)*5)/9+"\t");
+                 }
+                 System.out.println();
+             
+             
+                }
+        }
+}
+
+
