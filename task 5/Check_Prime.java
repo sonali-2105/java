caca80bdb201230dbe64937510a8a1abb6791ab4
sonali <sonali_chichghare@vistasoftwares.com>
2020-02-17 17:17:21 +0530

    add new task of java

---
+++ task 5/Check_Prime.java
@@ -0,0 +1,10 @@
+public class Check_Prime{
+     public static void main(String[] args){
+        int num=893;
+         if(num%2==0){
+            System.out.println("Entered number is not a prime number");
+        }else{
+            System.out.println("Entered number is  a prime number");
+        }
+    }
+}
