94ba48ff86810af9776db6709467478b1abb85f4
sonali <sonali_chichghare@vistasoftwares.com>
2020-02-18 11:24:31 +0530

    add new java notes and task

--- task 5/Check_Prime.java
+++ task 5/Check_Prime.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
 public class Check_Prime{
      public static void main(String[] args){
-        int num=893;
+        Scanner s =new Scanner(System.in);
+         System.out.print("Enter a number : ");
+        int num=s.nextInt();
          if(num%2==0){
-            System.out.println("Entered number is not a prime number");
+            System.out.print("Entered number is not a prime number");
         }else{
-            System.out.println("Entered number is  a prime number");
+            for(int i=2; i<=num; i++){
+                if(num%i==0){
+                    System.out.println("Entered number is a prime number");
+                }
+            }
+
+            
         }
     }
 }
