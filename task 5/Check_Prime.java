25237a082c1251995da9aefe3250b04ed0e0b664
sonali <sonali_chichghare@vistasoftwares.com>
2020-02-18 13:30:35 +0530

    add new task in java file

--- task 5/Check_Prime.java
+++ task 5/Check_Prime.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
 public class Check_Prime{
      public static void main(String[] args){
-        Scanner s =new Scanner(System.in);
+        Scanner sc =new Scanner(System.in);
          System.out.print("Enter a number : ");
-        int num=s.nextInt();
+        int num= sc.nextInt();
          if(num%2==0){
             System.out.print("Entered number is not a prime number");
         }else{
             for(int i=2; i<=num; i++){
                 if(num%i==0){
                     System.out.println("Entered number is a prime number");
+                    break;
                 }
             }
 
             
         }
+        sc.close();
     }
 }
---
+++ task 5/Sum_Prime.java
@@ -0,0 +1,26 @@
+import java.util.scanner;
+
+public class Sum_Prime{
+    public static void main(String[] args){
+        
+        int i, num, count, sum=0;
+        for(num=1; num<=30;num++)
+        {
+            count=0;
+            for(i=2; i<=num/2; i++)
+            {
+                if(num% i == 0)
+                {
+                    count++;
+                    break;
+
+                }
+            }
+            if(count==0 && num!=1)
+            {
+                sum = sum + num;
+            }
+        }
+        System.out.println("\n The sum of first ten prime numbers= "+sum);
+    }
+}
