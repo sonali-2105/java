e09542dc8876f8ab1a7ce0523b7b9e17367cf432
sonali <sonali_chichghare@vistasoftwares.com>
2020-02-20 14:08:19 +0530

    change in two task files

--- task 4/Temperature.java
+++ task 4/Temperature.java
@@ -1,11 +1,23 @@
 public class Temperature{
     public static void main(String[] args){
-        int Fahrenheit[]=new int[5];
+        int Fahrenheit[]=new int[16];
         Fahrenheit[0]=1;//initialization  
         Fahrenheit[1]=20;
         Fahrenheit[2]=40;
         Fahrenheit[3]=60;
         Fahrenheit[4]=80;
+        Fahrenheit[5]=100;
+        Fahrenheit[6]=120;
+        Fahrenheit[7]=140;
+        Fahrenheit[8]=160;
+        Fahrenheit[9]=180;
+        Fahrenheit[10]=200;
+        Fahrenheit[11]=220;
+        Fahrenheit[12]=240;
+        Fahrenheit[13]=260;
+        Fahrenheit[14]=280;
+        Fahrenheit[15]=300;
+
         
         for(int i=0; i<=Fahrenheit.length; i++){
             int Celsius = (((Fahrenheit[i] - 32)*5)/9);
--- task 5/Check_Prime.java
+++ task 5/Check_Prime.java
@@ -1,21 +1,35 @@
-import java.util.Scanner;
 public class Check_Prime{
-     public static void main(String[] args){
-        Scanner sc =new Scanner(System.in);
-         System.out.print("Enter a number : ");
-        int num= sc.nextInt();
-         if(num%2==0){
-            System.out.print("Entered number is not a prime number");
-        }else{
-            for(int i=2; i<=num; i++){
-                if(num%i==0){
-                    System.out.println("Entered number is a prime number");
-                    break;
-                }
-            }
+    
+     public static void main(String[] args)
+     {
+         int i, count=0, num=179;
+         
+         
+       // System.out.print("Enter a number : ");
+         
+         
+         for(i=2; i<=num/2; i++)
+         {
+              if(num%i ==0)
+              {
+                  count++;
+                  break;
+              }
+         }
+         if(count ==0 && num!=1)
+         {
+             System.out.println(num + " is a Prime Number");
+         }
+         else
+         {
+             System.out.println(num + " is not a Prime Number");
+         }
 
-            
-        }
-        sc.close();
+          
+             
+         
+        
     }
 }
+
+
